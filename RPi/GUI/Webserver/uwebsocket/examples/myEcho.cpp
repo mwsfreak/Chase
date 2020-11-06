62469402c0b0653782ea0668434efa4b9b81d53d
Your Name <you@example.com>
2020-11-06 09:33:47 +0100

    ..

--- RPi/GUI/Webserver/uwebsocket/examples/myEcho.cpp
+++ RPi/GUI/Webserver/uwebsocket/examples/myEcho.cpp
@@ -5,30 +5,32 @@
 #include <unistd.h>
 #include <uWS/uWS.h>
 #include <thread>
-#include <poll.h>             // polling UART for Data to read
+#include <poll.h>
 #include "../src/json.hpp"
-using json = nlohmann::json;
 
-#define UART_BYTES = 3
-#define timeout_msecs = 10 // Polling wait time milisecounds
-struct pollfd fds[1]; // Arry for file descriptors to be polled
+
+using json = nlohmann::json;
+struct pollfd src[1];
+#define UART_BYTES 3
 
 const char START_COMMAND = 0x01;
 const char STOP_COMMAND = 0x02;
+const char TEST = 0x50;
 
-bool gameRunning = false; 
+int uartInit();
+int uartSend(char command);
 
-
+bool gameRunning = false;
 
 struct Data
 {
   uWS::Hub &h;
   void operator()(uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
     std::string messageString(message, length);
-    
+
     // When recieving messages from Webinterface, check for JSON package or normal text message
     if ((messageString.front() == '{' && messageString.back() == '}') || //JSON Package
-      (messageString.front() == '[' && messageString.back() == ']')) 
+      (messageString.front() == '[' && messageString.back() == ']'))
     {
       json receivedJson = json::parse(messageString);
       std::cout << "JSON: " << receivedJson << std::endl;
@@ -39,19 +41,23 @@ struct Data
         if (gameRunning) {
           std::cout << "Game started" << std::endl;
           uartSend(START_COMMAND);
+
         } else {
           std::cout << "Game stopped" << std::endl;
           uartSend(STOP_COMMAND);
+
         }
 
       }
-    } 
-    else                                                                 // Simple text 
+    }
+    else                                                                 // Simple text
     {
-      std::cout << "TEXT: " << messageString << std::endl;     
+      std::cout << "TEXT: " << messageString << std::endl;
+      uartSend(TEST);
+
     }
       //DEBUG: Echo message
-      ws->send(message, length, opCode); 
+      ws->send(message, length, opCode);
     }
 };
 
@@ -62,7 +68,7 @@ void async(uWS::Hub* h)
     {"gameCommand" , "penalty"},  //
     {"index", 1}                  //
   };                              //
-  
+
 
   std::ostringstream ss2;         // JSON Package for AVGtime Update
   json avgJson = {                //
@@ -73,68 +79,52 @@ void async(uWS::Hub* h)
 
   for(;;)
   {
+    sleep(2);
+    std::ostringstream ss;
     int ret;
     char readBuf[UART_BYTES];
-    sleep(2);
 
-    std::ostringstream ss;
-    ss << "Broadcasting";
-    
     if (gameRunning) {
+      // Check for Data from PSoC
+      ret = poll(src, 1, 5000);  // Params: 1. file descriptor array, 2. fil descripter index, 3. timeout
+
+      ss << "Spillet er igang vi Afventer UART beskeder... revents: " << src[0].revents << " Should be: " << POLLIN;
       h->broadcast(ss.str().c_str(),ss.str().length(), uWS::OpCode::TEXT);
 
-      // Check for Data from PSoC
-      ret = poll(fds, 1, timeout_msecs);  // Params: 1. file descriptor array, 2. fil descripter index, 3. timeout
-      if (ret > 0) {                      
-        if (fds[0].revents & POLLIN) {    // New Data recieved
-          numRead = read(fds[0].fd, readBuf, UART_BYTES);
-          if ( numRead < readCnt)
-          {
-            printf("Failed to read %d, only read: %d\n\n",readCnt, numRead);
-          }
-          // Check read data
-          penJson.at("index") = readBuf[0];
-          avgJson.at("index") = readBuf[1];
-          avgJson.at("time") = readBuf[2];
-          // Opdate packages
-          ss1 << penJson;
-          ss2 << avgJson;
-          // boardcast packages
-          h->broadcast(ss1.str().c_str(),ss1.str().length(), uWS::OpCode::TEXT);
-          h->broadcast(ss2.str().c_str(),ss2.str().length(), uWS::OpCode::TEXT);
+      if(ret == -1)
+      {
+        std::cout << "Polling Fucked up..!" << std::endl;
+      }
+      else if (src[0].revents & POLLIN)
+      {    // New Data recieved
+        int numRead = read(src[0].fd, readBuf, UART_BYTES);
+        if ( numRead < UART_BYTES)
+        {
+          std::cout << "Failed to read" << UART_BYTES <<", only read: " << numRead << std::endl << std::endl ;
         }
+        // Check read data
+        std::cout << "Read Complete: " <<readBuf[0] << " 2: " << readBuf[1] << " 3: " << readBuf[2] << std::endl;
+        ss << "Vi har læst data...";
+        h->broadcast(ss.str().c_str(),ss.str().length(), uWS::OpCode::TEXT);
+        penJson.at("index") = readBuf[0];
+        avgJson.at("index") = readBuf[1];
+        avgJson.at("time") = readBuf[2];
+        // Opdate packages
+        ss1 << penJson;
+        ss2 << avgJson;
+        // boardcast packages
+        h->broadcast(ss1.str().c_str(),ss1.str().length(), uWS::OpCode::TEXT);
+        h->broadcast(ss2.str().c_str(),ss2.str().length(), uWS::OpCode::TEXT);
+      }
+      else
+      {
+        std::cout << "Timepout " << std::endl;
       }
     }
-  }
-
-  // noget med at sende start og stop commandoer
-}
-
-
-
-void updateRecieve(uWS::Hub* h)
-{  
-  std::ostringstream ss1;         // JSON Package for Penalty Update
-  json penJson= {                 //
-    {"gameCommand" , "penalty"},  //
-    {"index", 1}                  //
-  };                              //
-  ss1 << penJson;                 //
-
-  std::ostringstream ss2;         // JSON Package for AVGtime Update
-  json avgJson = {                //
-    {"gameCommand" , "AVGtime"},  //
-    {"index", 1}                  //  
-};                                //
-  ss2 << avgJson;                 //
-
-  for(;;)
-  {
-    sleep(2);
-    
-    if (gameRunning) {
-      h->broadcast(ss1.str().c_str(),ss1.str().length(), uWS::OpCode::TEXT);
-      h->broadcast(ss2.str().c_str(),ss2.str().length(), uWS::OpCode::TEXT);
+    else
+    {
+      ss << "Broadcasting 1: " << readBuf[0] << " 2: " << readBuf[1] << " 3: " << readBuf[2];
+      h->broadcast(ss.str().c_str(),ss.str().length(), uWS::OpCode::TEXT);
     }
   }
 }
@@ -142,25 +132,39 @@ void updateRecieve(uWS::Hub* h)
 
 int main()
 {
+  int serial_fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
+  src[0].fd = serial_fd;
+  src[0].events = POLLIN;
+  src[0].revents = 0;
   // WebSocket Init
   uWS::Hub hub;
   Data d { hub };
   hub.onMessage(d);
-  // UART Init
-  fds[0].fd = open("/dev/ttyAMA0", O_RDWR);
-    if (fds[0].fd == -1) {
-      printf("Failed to open, with error: %s\n\n", strerror(errno));
-    }
-  fds[0].events = POLLOUT | POLLIN; // POLLIN -> New data ready, POLLOUT -> ready to write
-  
   if (hub.listen(3000)) {
-    
+
     std::thread th(async, &hub);
 
     hub.run();
   }
 }
 
+
+int uartInit() {
+  int fd;
+  fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
+  if (fd == -1) {
+    printf("Failed to open, with error: %s\n\n", strerror(errno));
+  }
+  return fd;
+}
+
+
+int uartSend(char command) {
+  int fd = uartInit();
+  write(fd, &command, 1);
+  close(fd);
+  return 0;
+}
 
   // Som lambda - optional!
   /*
@@ -192,7 +196,7 @@ int uartInit() {
 int uartClose(int fd) {
   int status = close(fd);
   if (status == -1) {
-    printf("Failed to close, with error: %s \n\n",strerror(errno));   
+    printf("Failed to close, with error: %s \n\n",strerror(errno));
   }
   return status;
 }
