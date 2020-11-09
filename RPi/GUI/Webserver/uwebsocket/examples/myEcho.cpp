ca3c7cd4eba014c2cc3548e39c2eb19139943484
Magnus Bisgård Franks <201803510@post.au.dk>
2020-11-09 17:41:25 +0100

    Fixed json message and player indexing.
    Fixed json messages getting appended for each new message. Json message is now reinitialised before being updated.
    Changed player indexing to 0-7 instead of 1-8, added fault value -1.
    Various cleanup.

--- RPi/GUI/Webserver/uwebsocket/examples/myEcho.cpp
+++ RPi/GUI/Webserver/uwebsocket/examples/myEcho.cpp
@@ -3,17 +3,13 @@
 #include <sstream>
 #include <fcntl.h>
 #include <unistd.h>
-
 #include <errno.h>
 #include <termios.h>
-
 #include <uWS/uWS.h>
 #include <thread>
 #include "../src/json.hpp"
 using namespace std;
 using json = nlohmann::json;
-
-// 8 bit UART beskeder: bit 0 - 2 -> spiller strafpoint, bit 3 - 5 -> spiller AVGT, bit 6 - 7  -> time
 
 const char START_COMMAND = 0x01;
 const char STOP_COMMAND = 0x02;
@@ -63,9 +59,6 @@ void async(uWS::Hub* h)
 {
   int counter = 0;
   
-  ostringstream ss1;
-//ostringstream ss2;
-
   json package = {
     {"playerPenaltyIndex" , 0},
     {"playerAvgIndex", 0},
@@ -74,27 +67,27 @@ void async(uWS::Hub* h)
 
   for(;;)
   {
-    sleep(1);
-
     char buffer[4] = {0};
 
     if (uartReceive(buffer) >= 0) {
       
       //input validation
       cout << "Buffer[0] = " << buffer[0] + 0;
-      if (buffer[0] >= 1 && buffer[0] <= 8) {
+      if (buffer[0] >= 0 && buffer[0] <= 7) {
         package["playerPenaltyIndex"] = buffer[0];
-        cout << ", playerPenaltyIndex = " << package["playerPenaltyIndex"];
+      } else {
+        package["playerPenaltyIndex"] = -1;
       }
-      cout << endl;
+      cout << ", playerPenaltyIndex = " << package["playerPenaltyIndex"] << endl;
 
 
       cout << "Buffer[1] = " << buffer[1] + 0;
-      if (buffer[1] >= 1 && buffer[1] <= 8) {
+      if (buffer[1] >= 0 && buffer[1] <= 7) {
         package["playerAvgIndex"] = buffer[1];
-        cout << ", playerAvgIndex = " << package["playerAvgIndex"];
+      } else {
+        package["playerAvgIndex"] = -1;
       }
-      cout << endl;
+      cout << ", playerAvgIndex = " << package["playerAvgIndex"] << endl;
 
 
       uint16_t playerTime = (buffer[2] << 8) | buffer[3];
@@ -111,16 +104,16 @@ void async(uWS::Hub* h)
     }
 
     cout << "Printing package in JSON format" << endl << package.dump(4) << endl;
-
-    ss1 << package;
-
+    
+    ostringstream ss1;
+    ss1 << package;    
+    
     ostringstream ss;
     ss << "Number of broadcasts #" << counter++;
     
     if (gameRunning) {
       h->broadcast(ss.str().c_str(),ss.str().length(), uWS::OpCode::TEXT);
       h->broadcast(ss1.str().c_str(),ss1.str().length(), uWS::OpCode::TEXT);
-//    h->broadcast(ss2.str().c_str(),ss2.str().length(), uWS::OpCode::TEXT);
     }
   }
 }
