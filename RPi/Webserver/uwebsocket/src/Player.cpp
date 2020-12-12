cede5855f115961441d3a74be21db21dba9e77c0
mwsfreak <m.franks@live.dk>
2020-12-12 22:09:35 +0100

    Added comments

--- RPi/Webserver/uwebsocket/src/Player.cpp
+++ RPi/Webserver/uwebsocket/src/Player.cpp
@@ -1,5 +1,12 @@
+/*    
+* Title:        Player.cpp
+* Description:  Player class source file
+* Authors:      Allan Lassesen, Magnus Bisgård Franks
+*/
+
 #include "Player.h"
 
+/* Calculate a players average time */
 void Player::newTime(int time) {
     if (time > 0) {
         sum_ += time;
@@ -8,27 +15,31 @@ void Player::newTime(int time) {
     }
 }
 
+/* Add penalty point to a player */
 int Player::addPenalty(void) {
     penalty_++;
     return penalty_;
 }    
 
+/* Get a players penalty points */
 int Player::getPenalty(void) const {
     return penalty_;
 }
 
+/* Get a players average time */
 float Player::getAvgTime(void) const {
     return avgTime_;
 }
 
+/* Reset a players data, and set a new name for the player */
 void Player::resetPlayer(string name) {
     name_ = name;
-    penalty_ = 0;   // Reset values when new name is applied
-    avgTime_ = 0;   //
-    sum_ = 0;       //
-    
+    penalty_ = 0;
+    avgTime_ = 0;
+    sum_ = 0;
 }
 
+/* Get a players name */
 string Player::getName() const {
     return name_;
 }
--- RPi/Webserver/uwebsocket/src/Player.h
+++ RPi/Webserver/uwebsocket/src/Player.h
@@ -1,3 +1,9 @@
+/*    
+* Title:        Player.h
+* Description:  Player class header file
+* Authors:      Allan Lassesen, Magnus Bisgård Franks
+*/
+
 #pragma once
 
 #include <string>
@@ -11,13 +17,13 @@ public:
     int addPenalty();
     int getPenalty() const;
     float getAvgTime() const;
-    void resetPlayer(string name);      // Changed to resetPlayer, and reset private data, when new name is set
+    void resetPlayer(string name);
     string getName() const;
 
 private:
-    string name_{"Unknown"};
-    int penalty_{0};
-    float avgTime_{0};
-    uint32_t sum_{0};               //Big to avoid overflow
-    int timeCounter_{0};
+    string name_{"Unknown"};        //Player name
+    int penalty_{0};                //Penalty points
+    float avgTime_{0};              //Calculated average time in seconds
+    uint32_t sum_{0};               //Sum of all player times in 1/100th seconds - 32 bytes to avoid overflow
+    int timeCounter_{0};            //Number of times to calculate average from
 };
